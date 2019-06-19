729d00986bb619af1c5f6c68427cb034672e4022
Cristina Alekseeva <cristina.alekseeva@gmail.com>
2019-06-19 11:47:18 -0700

    added displayMovie function

--- liri.js
+++ liri.js
@@ -3,10 +3,8 @@ var keys = require("./keys.js");
 var fs = require("fs");
 var axios = require("axios");
 var moment = require("moment");
-var action = "concert-this";
-var input = "vampire weekend";
-
-input.replace(" ", "%20");
+var action = process.argv[2];
+var input = process.argv[3];
 
 switch (action) {
   case "concert-this":
@@ -24,6 +22,7 @@ switch (action) {
 }
 
 function displayConsert() {
+  input.replace(" ", "%20");
   axios
     .get(
       "https://rest.bandsintown.com/artists/" +
@@ -57,4 +56,44 @@ function displayConsert() {
     });
 }
 
-//   var spotify = new Spotify(keys.spotify);
+function displaySong() {
+  //   var spotify = new Spotify(keys.spotify);
+}
+
+function displayMovie() {
+  var url;
+  if (input !== undefined) {
+    input.replace(" ", "+");
+    url =
+      "http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy";
+  } else {
+    url = "http://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&apikey=trilogy";
+  }
+
+  axios
+    .get(url)
+    .then(function(response) {
+      var result = response.data;
+      //console.log(response.data);
+      console.log("title: " + result.Title);
+      console.log("year: " + result.Year);
+      console.log("IMDB Rating: " + result.Ratings[0].Value);
+      console.log("Rotten Tomatoes Rating: " + result.Ratings[1].Value);
+      console.log("country: " + result.Country);
+      console.log("language: " + result.Language);
+      console.log("plot: " + result.Plot);
+      console.log("actors: " + result.Actors);
+    })
+    .catch(function(error) {
+      if (error.result) {
+        console.log(error.result.data);
+        console.log(error.result.status);
+        console.log(error.result.headers);
+      } else if (error.request) {
+        console.log(error.request);
+      } else {
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    });
+}
