10b42fdbe4821d7d1af0371d828f0d471c5379e0
Cristina Alekseeva <cristina.alekseeva@gmail.com>
2019-06-19 21:47:38 -0700

    added displaySong function

--- liri.js
+++ liri.js
@@ -39,7 +39,8 @@ function displayConsert() {
         console.log(
           "location: " + result[i].venue.city + "," + result[i].venue.country
         );
-        console.log("date: " + dateFormated);
+        console.log("date: " + dateFormated + "\n");
+        console.log("_______________________________");
       }
     })
     .catch(function(error) {
@@ -57,7 +58,26 @@ function displayConsert() {
 }
 
 function displaySong() {
-  //   var spotify = new Spotify(keys.spotify);
+  var Spotify = require("node-spotify-api");
+  var spotify = new Spotify(keys.spotify);
+  input.replace(" ", "%20");
+  spotify.search({ type: "track", query: input }, function(err, data) {
+    if (err) {
+      return console.log("Error occurred: " + err);
+    }
+
+    console.log(JSON.stringify(data.tracks.items, null, "\t"));
+
+    for (var i = 0; i < data.tracks.items.length; i++) {
+      var result = data.tracks.items[i];
+
+      console.log("Artist(s): +" + result.artists[0].name);
+      console.log("The song's name: " + result.name);
+      console.log("A preview link of the song: " + result.preview_url);
+      console.log("The album: " + result.album.name + "\n");
+      console.log("_______________________________");
+    }
+  });
 }
 
 function displayMovie() {
@@ -96,4 +116,21 @@ function displayMovie() {
       }
       console.log(error.config);
     });
+}
+
+function displayRandom() {
+  fs.readFile("random.txt", "utf8", function(error, data) {
+    if (error) {
+      return console.log(error);
+    }
+    console.log(data);
+
+    var dataArr = data.split("\n");
+
+    for (var i = 0; i < dataArr.length; i++) {
+      action = dataArr[i][0];
+      input = dataArr[i][1];
+      return action + " " + input;
+    }
+  });
 }
